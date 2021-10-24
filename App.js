d130f3cb7934b79442e3bf0de4a1079d62506b6a
Esteban <cehernandezco@gmail.com>
2021-10-24 19:19:10 +1100

    new files
    elements included
    input
    button
    flatList
    emptyFlatlist

--- App.js
+++ App.js
@@ -1,12 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, TextInput } from 'react-native';
+import Constants from 'expo-constants';
+
+import {Item} from './components/Item'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import EmptyList from './components/EmptyList';
 
 export default function App() {
+
+  const [ data, setData ] = useState([])
+  const [ validInput, setValidInput ] = useState(false)
+  const [ input, setInput ] = useState()
+  const [ appInit, setAppInit ] = useState(true)
+
+  useEffect( () => {
+    if(appInit ){
+      getData()
+      setAppInit(false)
+    }else{
+      storeData()
+    }
+  }, [data])
+
+  const onTextChange = (value) => {
+    setInput(value)
+    if(value.length >= 3){
+      setValidInput(true)
+    }else{
+      setValidInput(false)
+    }
+  }
+
+  const onSubmit = (event) => {
+    const id = new Date().getTime().toString()
+    const item = { id: id, name: input}
+    setData( [...data, item])
+    setInput(null)
+    setValidInput(false)
+    
+  }
+
+  const onDelete = (id) => {
+    let items = [...data]
+    let newData = items.filter( (item) => {
+      if( item.id !== id){
+        return item
+      }
+    })
+    setData(newData)
+    
+  }
+
+  const storeData = async() => {
+    const stringified = JSON.stringify( data )
+    try{
+      await AsyncStorage.setItem( "listData" , stringified )
+    }    
+    catch(error){
+      console.log(error)
+    }  
+  }
+
+  const getData = async () => {
+    try {
+      const stringified = await AsyncStorage.getItem("listData")
+      console.log( stringified )
+      setData( (stringified !== null) ? JSON.parse(stringified) : [] )
+    } catch (error) {
+      console.log( error )
+    }
+  }
+
+  
+
+  const Renderer = ({item}) => ( <Item text={item.name} delete={onDelete} id={item.id} /> )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList data={data} 
+      keyExtractor={ (item) => item.id } renderItem={Renderer}
+      ListEmptyComponent={() => <EmptyList/>} />
+      <View style={styles.footer}>
+        <TextInput 
+          style={styles.input} 
+          placeholder="min 3 characters"
+          onChangeText={onTextChange}
+          value={input}/>
+        <TouchableOpacity 
+          style={(validInput)? styles.button : styles.buttonDisabled}
+          disabled = {(validInput) ? false : true }
+          onPress={onSubmit}>
+          <Text style={ (validInput)? styles.buttonText : styles.buttonTextDisabled}>Add to list</Text>
+        </TouchableOpacity>
+      </View>
+      
     </View>
   );
 }
@@ -14,8 +102,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#daddf2',
+    marginTop: Constants.statusBarHeight
+  },
+  footer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom:20,
+  },
+  input:{
+    backgroundColor: '#d6ebff',
+    fontSize: 20,
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+    padding: 5,
+    flex: 1,
+  },
+  button: {
+    backgroundColor: '#0000FF',
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: '#FFF',
+    padding: 10,
+
+  },
+  buttonTextDisabled: {
+    color: '#000',
+    padding: 10,
+
+  },
+  buttonDisabled: {
+    backgroundColor: '#C0C0C0',
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
   },
 });
---
+++ components/EmptyList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image} from 'react-native'
+
+export default function EmptyList() {
+    return (
+        <View style={styles.container}> 
+            <Image source={ require('../assets/mirage-list-is-empty.png') } style={styles.emptyImage} />
+            <Text style={styles.emptyText}>Add an Item</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop: 40,
+        paddingTop: 20,
+        display:'flex',
+        flex: 1,
+        alignItems:'center',
+        justifyContent: 'space-around',
+
+    },
+    emptyImage:{
+        width: 350,
+        height:200,
+    },
+    emptyText:{
+        color:"#fff",
+        fontFamily:"poppins-bold",
+        marginTop: 30,
+        fontSize: 30,
+    },
+})
